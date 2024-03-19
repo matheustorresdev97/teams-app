49da8354914659ac90bd1f5c496d0ef469313ac0
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-03-19 12:10:36 -0300

    :sparkles: feat: Componente Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -12,15 +13,15 @@ export function NewGroup() {
       <Content>
         <Icon />
 
-        <Highlight 
+        <Highlight
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Button 
-          title="Criar"
-        />
+        <Input />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
-  )
+  );
 }

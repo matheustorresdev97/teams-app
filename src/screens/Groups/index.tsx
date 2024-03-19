e3d1aee378935d56eb4c0ab6dba777995c4cebd0
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-03-19 11:52:08 -0300

    :sparkles: feat: Componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,16 @@
-import { Container } from './styles';
+import { GroupCard } from "@components/GroupCard";
+import { Container } from "./styles";
 
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
   return (
     <Container>
       <Header />
-      <Highlight 
-        title="Turmas"
-        subtitle="jogue com a sua turma"
-      />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <GroupCard title="Galera do NTI" />
     </Container>
   );
 }

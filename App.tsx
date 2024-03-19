772ec9ab844b0ee28d2a45a6110c12ab428bcddd
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-03-19 12:09:18 -0300

    :sparkles: feat: Screen NewGroup

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components/native'; 
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import theme from "./src/theme";
 
+import { Loading } from "@components/Loading";
+
+import { NewGroup } from "@screens/NewGroup";
 import { Groups } from "@screens/Groups";
-import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-  
+
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar 
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        { fontsLoaded ? <Groups/> : <Loading/> }
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }

60312704ca9dc4f88c1e623d7fd4fddbcc88b486
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-03-20 08:12:58 -0300

    :sparkles: feat: Implementando o voltar

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
@@ -6,6 +8,12 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group: "Rocket" });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -18,11 +26,9 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input
-          placeholder="Nome da turma"
-           />
+        <Input placeholder="Nome da turma" />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );

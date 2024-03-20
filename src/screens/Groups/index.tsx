f856ca8ca6cd9a1144fc7f41ec69a8d95d417a1c
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-03-20 08:42:57 -0300

    :sparkles: feat: Abrindo novas turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,10 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback } from "react";
 import { FlatList } from "react-native";
 import { Container } from "./styles";
 
-import { useNavigation, useFocusEffect } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
-import { groupsGetAll } from '@storage/group/groupsGetAll';
+import { groupsGetAll } from "@storage/group/groupsGetAll";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -15,24 +15,30 @@ import { Button } from "@components/Button";
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
 
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   function handleNewGroup() {
-    navigation.navigate('new')
+    navigation.navigate("new");
   }
 
   async function fetchGroups() {
     try {
       const data = await groupsGetAll();
-      setGroups(data)
+      setGroups(data);
     } catch (error) {
       console.log(error);
     }
   }
 
-  useFocusEffect(useCallback(() => {
-    fetchGroups()
-  },[]))
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <Container>
@@ -42,14 +48,16 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
 
-      <Button title="Criar nova turma"   onPress={handleNewGroup} />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }

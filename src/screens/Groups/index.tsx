684038dfc43f72ad939fa8c66c7501131b38118e
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-03-20 08:35:32 -0300

    :sparkles: feat: Identificando foco na tela e trazendo os grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useCallback } from 'react';
 import { FlatList } from "react-native";
 import { Container } from "./styles";
 
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
+
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -18,6 +20,19 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate('new')
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups()
+  },[]))
 
   return (
     <Container>

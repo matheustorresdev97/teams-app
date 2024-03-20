22e6b4c2ca7aadcab046e2f211083a95bd0e8b9a
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-03-20 08:40:08 -0300

    :sparkles: feat: Tratando exceções e Criando classe de erros personalizadas

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { Alert } from "react-native";
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
@@ -9,6 +10,8 @@ import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { AppError } from "@utils/AppError";
+
 
 export function NewGroup() {
   const [group, setGroup] = useState("");
@@ -17,10 +20,20 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+
+      if(group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if(error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     }
   }
 
